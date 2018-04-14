f030f1c23eb281a9939de121d9b065798600530f
rigaya <rigaya34589@live.jp>
2018-04-14 19:45:47 +0900

    x265のビルドをmsys2のgccビルドに。 x64のみmultilib/fprofiled対応。 fltoはうまくいかないため、見送った。

---
+++ x265/build_x265.sh
@@ -0,0 +1,231 @@
+#!/bin/sh
+#msys2用x265ビルドスクリプト
+#pacman -S base-devel mingw-w64-i686-toolchain mingw-w64-x86_64-toolchain p7zip git nasm p7zip
+#そのほかにcmake, mercurial(windows版)のインストールが必要
+NJOBS=$(($NUMBER_OF_PROCESSORS>16?16:$NUMBER_OF_PROCESSORS))
+BUILD_DIR=$HOME/build_x265
+#cmake.exeのある場所
+CMAKE_DIR="/C/Program Files/CMake/bin"
+#hg.exeのある場所
+HG_DIR="/C/Program Files/Mercurial"
+#プロファイル用のソース
+Y4M_PATH=$HOME/sakura_op_cut.y4m
+
+UPDATE_X265="TRUE"
+BUILD_12BIT="ON"
+BUILD_10BIT="ON"
+PROFILE_GEN_CC="-fprofile-generate"
+PROFILE_GEN_LD="-fprofile-generate"
+PROFILE_USE_CC="-fprofile-use"
+PROFILE_USE_LD="-fprofile-use"
+
+export PATH="${CMAKE_DIR}:${HG_DIR}:$PATH"
+
+mkdir -p $BUILD_DIR
+mkdir -p $BUILD_DIR/src
+cd $BUILD_DIR/src
+
+# [ "x86", "x64" ]
+if [ $MSYSTEM = "MINGW32" ]; then
+    TARGET_ARCH="x86"
+else
+    TARGET_ARCH="x64"
+fi
+
+if [ $TARGET_ARCH = "x64" ]; then
+    BUILD_CCFLAGS="-O3 -msse2 -I${INSTALL_DIR}/include"
+    BUILD_LDFLAGS="-static -static-libgcc -static-libstdc++ -L${INSTALL_DIR}/lib"
+elif [ $TARGET_ARCH = "x86" ]; then
+    BUILD_CCFLAGS="-m32 -msse2 -I${INSTALL_DIR}/include" 
+    BUILD_LDFLAGS="-static -static-libgcc -static-libstdc++ -L${INSTALL_DIR}/lib"
+    #x86向けには高ビット深度版はビルドしない
+    BUILD_12BIT="OFF"
+    BUILD_10BIT="OFF"
+    #x86向けには、プロファイル最適化はスキップする
+    PROFILE_GEN_CC=""
+    PROFILE_GEN_LD=""
+    PROFILE_USE_CC=""
+    PROFILE_USE_LD=""
+else
+    echo "invalid TARGET_ARCH: ${TARGET_ARCH}"
+    exit
+fi
+
+#--- ソースのダウンロード ---------------------------------------
+if [ -d "x265" ]; then
+    if [ $UPDATE_X265 != "FALSE" ]; then
+        cd x265
+        make uninstall && make distclean &> /dev/null
+        hg pull
+        cd ..
+    fi
+else
+    UPDATE_X265=TRUE
+    hg clone https://bitbucket.org/multicoreware/x265
+fi
+
+# --- 出力先を準備 --------------------------------------
+if [ ! -d $BUILD_DIR/$TARGET_ARCH ]; then
+    mkdir $BUILD_DIR/$TARGET_ARCH
+fi
+cd $BUILD_DIR/$TARGET_ARCH
+if [ -d x265 ]; then
+    rm -rf x265
+fi
+cp -r ../src/x265 .
+
+# --- バージョン情報 ----------------------------------------------
+cd $BUILD_DIR/$TARGET_ARCH/x265
+X265_VER=`hg log -r. --template "{latesttag}"`
+X265_VER=${X265_VER}+`hg log -r. --template "{latesttagdistance}"`
+echo "build x265 ${X265_VER}"
+
+# --- ビルド ----------------------------------------------
+cd $BUILD_DIR/$TARGET_ARCH/x265/build/msys
+
+X265_EXTRA_LIB=""
+
+mkdir -p 8bit
+
+if [ "${PROFILE_GEN_CC}" != "" ]; then
+
+    CMAKE_PROFILE_ARG="-DFPROFILE_GENERATE=ON"
+
+    if [ $BUILD_12BIT = "ON" ]; then
+        mkdir -p 12bit
+        cd 12bit
+        #fprofile-generateするためには、-fprofile-generateを
+        #CMAKE_C_FLAGS/CMAKE_CXX_FLAGSだけでなく、CMAKE_EXE_LINKER_FLAGSに渡す必要がある
+        cmake -G "MSYS Makefiles" ../../../source \
+            -DHIGH_BIT_DEPTH=ON \
+            -DEXPORT_C_API=OFF \
+            -DENABLE_SHARED=OFF \
+            -DENABLE_CLI=OFF \
+            -DMAIN12=ON \
+            ${CMAKE_PROFILE_ARG} \
+            -DCMAKE_C_FLAGS="${BUILD_CCFLAGS} ${PROFILE_GEN_CC}" \
+            -DCMAKE_CXX_FLAGS="${BUILD_CCFLAGS} ${PROFILE_GEN_CC}" \
+            -DCMAKE_EXE_LINKER_FLAGS="${BUILD_LDFLAGS} ${PROFILE_GEN_LD}"
+        make -j${NJOBS}
+        cp libx265.a ../8bit/libx265_main12.a
+        X265_EXTRA_LIB="x265_main12"
+    fi
+
+    cd $BUILD_DIR/$TARGET_ARCH/x265/build/msys
+    if [ $BUILD_10BIT = "ON" ]; then
+        mkdir -p 10bit
+        cd 10bit
+        #fprofile-generateするためには、-fprofile-generateを
+        #CMAKE_C_FLAGS/CMAKE_CXX_FLAGSだけでなく、CMAKE_EXE_LINKER_FLAGSに渡す必要がある
+        cmake -G "MSYS Makefiles" ../../../source ../../../source \
+            -DHIGH_BIT_DEPTH=ON \
+            -DEXPORT_C_API=OFF \
+            -DENABLE_SHARED=OFF \
+            -DENABLE_CLI=OFF \
+            ${CMAKE_PROFILE_ARG} \
+            -DCMAKE_C_FLAGS="${BUILD_CCFLAGS} ${PROFILE_GEN_CC}" \
+            -DCMAKE_CXX_FLAGS="${BUILD_CCFLAGS} ${PROFILE_GEN_CC}" \
+            -DCMAKE_EXE_LINKER_FLAGS="${BUILD_LDFLAGS} ${PROFILE_GEN_LD}"
+        make -j${NJOBS}
+        cp libx265.a ../8bit/libx265_main10.a
+        X265_EXTRA_LIB="x265_main10;${X265_EXTRA_LIB}"
+    fi
+
+    cd $BUILD_DIR/$TARGET_ARCH/x265/build/msys/8bit
+    cmake -G "MSYS Makefiles" ../../../source \
+        -DEXTRA_LIB="${X265_EXTRA_LIB}" \
+        -DEXTRA_LINK_FLAGS=-L. \
+        -DLINKED_10BIT=${BUILD_10BIT} \
+        -DLINKED_12BIT=${BUILD_12BIT} \
+        -DENABLE_SHARED=OFF \
+        ${CMAKE_PROFILE_ARG} \
+        -DCMAKE_C_FLAGS="${BUILD_CCFLAGS} ${PROFILE_GEN_CC}" \
+        -DCMAKE_CXX_FLAGS="${BUILD_CCFLAGS} ${PROFILE_GEN_CC}" \
+        -DCMAKE_EXE_LINKER_FLAGS="${BUILD_LDFLAGS} ${PROFILE_GEN_LD}"
+
+    #強制的に完全な静的リンクにする
+    sed -i -e 's/Bdynamic/Bstatic/g' CMakeFiles/cli.dir/linklibs.rsp
+    make -j${NJOBS}
+
+    #profileのための実行はシングルスレッドで行う
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --preset faster
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --preset fast
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}"
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --preset slow
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --preset slower
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --output-depth 10 --preset faster
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --output-depth 10 --preset fast
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --output-depth 10
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --output-depth 10 --preset slow
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --output-depth 10 --preset slower
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --output-depth 12 --preset faster
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --output-depth 12 --preset fast
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --output-depth 12
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --output-depth 12 --preset slow
+    ./x265 --pools none --frame-threads 1 --lookahead-slices 0 --y4m -o /dev/nul "${Y4M_PATH}" --output-depth 12 --preset slower
+
+fi
+
+
+if [ "${PROFILE_GEN_CC}" = "" ]; then
+    CMAKE_PROFILE_ARG=""
+else
+    CMAKE_PROFILE_ARG="-DFPROFILE_GENERATE=OFF -DFPROFILE_USE=ON"
+fi
+
+X265_EXTRA_LIB=""
+
+cd $BUILD_DIR/$TARGET_ARCH/x265/build/msys
+if [ $BUILD_12BIT = "ON" ]; then
+    mkdir -p 12bit
+    cd 12bit
+    cmake -G "MSYS Makefiles" ../../../source \
+        -DHIGH_BIT_DEPTH=ON \
+        -DEXPORT_C_API=OFF \
+        -DENABLE_SHARED=OFF \
+        -DENABLE_CLI=OFF \
+        -DMAIN12=ON \
+        ${CMAKE_PROFILE_ARG} \
+        -DCMAKE_C_FLAGS="${BUILD_CCFLAGS} ${PROFILE_USE_CC}" \
+        -DCMAKE_CXX_FLAGS="${BUILD_CCFLAGS} ${PROFILE_USE_CC}" \
+        -DCMAKE_EXE_LINKER_FLAGS="${BUILD_LDFLAGS} ${PROFILE_USE_LD}"
+    make -j${NJOBS}
+    cp libx265.a ../8bit/libx265_main12.a
+    X265_EXTRA_LIB="x265_main12"
+fi
+
+cd $BUILD_DIR/$TARGET_ARCH/x265/build/msys
+if [ $BUILD_10BIT = "ON" ]; then
+    mkdir -p 10bit
+    cd 10bit
+    cmake -G "MSYS Makefiles" ../../../source \
+        -DHIGH_BIT_DEPTH=ON \
+        -DEXPORT_C_API=OFF \
+        -DENABLE_SHARED=OFF \
+        -DENABLE_CLI=OFF \
+        ${CMAKE_PROFILE_ARG} \
+        -DCMAKE_C_FLAGS="${BUILD_CCFLAGS} ${PROFILE_USE_CC}" \
+        -DCMAKE_CXX_FLAGS="${BUILD_CCFLAGS} ${PROFILE_USE_CC}" \
+        -DCMAKE_EXE_LINKER_FLAGS="${BUILD_LDFLAGS} ${PROFILE_USE_LD}"
+    make -j${NJOBS}
+    cp libx265.a ../8bit/libx265_main10.a
+    X265_EXTRA_LIB="x265_main10;${X265_EXTRA_LIB}"
+fi
+
+cd $BUILD_DIR/$TARGET_ARCH/x265/build/msys/8bit
+cmake -G "MSYS Makefiles" ../../../source \
+    -DEXTRA_LIB="${X265_EXTRA_LIB}" \
+    -DEXTRA_LINK_FLAGS=-L. \
+    -DLINKED_10BIT=${BUILD_10BIT} \
+    -DLINKED_12BIT=${BUILD_12BIT} \
+    -DENABLE_SHARED=OFF \
+    ${CMAKE_PROFILE_ARG} \
+    -DCMAKE_C_FLAGS="${BUILD_CCFLAGS} ${PROFILE_USE_CC}" \
+    -DCMAKE_CXX_FLAGS="${BUILD_CCFLAGS} ${PROFILE_USE_CC}" \
+    -DCMAKE_EXE_LINKER_FLAGS="${BUILD_LDFLAGS} ${PROFILE_USE_LD}"
+
+#強制的に完全な静的リンクにする
+sed -i -e 's/Bdynamic/Bstatic/g' CMakeFiles/cli.dir/linklibs.rsp
+make -j${NJOBS}
+
+strip -s $BUILD_DIR/$TARGET_ARCH/x265/build/msys/8bit/x265
---
+++ x265/build_x265_auto.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+BUILD_DIR=$HOME/build_x265
+#cmake.exe‚Ì‚ ‚éêŠ
+CMAKE_DIR="/C/Program Files/CMake/bin"
+#hg.exe‚Ì‚ ‚éêŠ
+HG_DIR="/C/Program Files/Mercurial"
+#“¯ŠúƒtƒHƒ‹ƒ_
+GOOGLE_DIR="/C/Users/rigaya/GoogleDrive/x265"
+ONEDRIVE_DIR="/C/Users/rigaya/OneDrive/x265"
+DROPBOX_DIR="/C/Users/rigaya/DropBox/x265"
+GPL_LICENSE_PATH="/C/Users/rigaya/GoogleDrive/txt/gplv2.txt"
+
+if [ $MSYSTEM = "MINGW32" ]; then
+    TARGET_ARCH="x86"
+else
+    TARGET_ARCH="x64"
+fi
+
+export PATH="${CMAKE_DIR}:${HG_DIR}:$PATH"
+
+./build_x265.sh
+
+cd $BUILD_DIR/src/x265
+X265_VER=`hg log -r. --template "{latesttag}"`
+X265_VER=${X265_VER}+`hg log -r. --template "{latesttagdistance}"`
+echo "build x265 ${X265_VER}"
+
+mkdir $BUILD_DIR/temp
+cp -r $BUILD_DIR/src/x265 $BUILD_DIR/temp/src
+rm -rf $BUILD_DIR/temp/src/x265/.hg
+
+7z a -t7z -mx=9 -mmt=off "$BUILD_DIR/temp/x265_${X265_VER}_src.7z" $BUILD_DIR/temp/src/
+
+cp -f $BUILD_DIR/$TARGET_ARCH/x265/build/msys/8bit/x265.exe $BUILD_DIR/$TARGET_ARCH/x265/build/msys/8bit/x265_${X265_VER}_${TARGET_ARCH}.exe
+7z a -tzip -mx=9 -mfb=256 -mpass=15 -mmt=off "$BUILD_DIR/temp/x265_latest_${TARGET_ARCH}.zip" $BUILD_DIR/$TARGET_ARCH/x265/build/msys/8bit/x265_${X265_VER}_${TARGET_ARCH}.exe ${GPL_LICENSE_PATH}
+
+cp -f "$BUILD_DIR/temp/x265_${X265_VER}_src.7z" "${GOOGLE_DIR}/src"
+cp -f "$BUILD_DIR/temp/x265_${X265_VER}_src.7z" "${ONEDRIVE_DIR}/src"
+cp -f "$BUILD_DIR/temp/x265_${X265_VER}_src.7z" "${DROPBOX_DIR}/src"
+
+cp -f "$BUILD_DIR/temp/x265_latest_${TARGET_ARCH}.zip" "${GOOGLE_DIR}"
+cp -f "$BUILD_DIR/temp/x265_latest_${TARGET_ARCH}.zip" "${ONEDRIVE_DIR}"
+cp -f "$BUILD_DIR/temp/x265_latest_${TARGET_ARCH}.zip" "${DROPBOX_DIR}"
+
+if [ $TARGET_ARCH = "x64" ]; then
+    cp -f "$BUILD_DIR/temp/x265_latest_${TARGET_ARCH}.zip" "${GOOGLE_DIR}/x265_latest_${TARGET_ARCH}_pgo.zip"
+    cp -f "$BUILD_DIR/temp/x265_latest_${TARGET_ARCH}.zip" "${ONEDRIVE_DIR}/x265_latest_${TARGET_ARCH}_pgo.zip"
+    cp -f "$BUILD_DIR/temp/x265_latest_${TARGET_ARCH}.zip" "${DROPBOX_DIR}/x265_latest_${TARGET_ARCH}_pgo.zip"
+fi
+
+cp -f "$BUILD_DIR/temp/x265_latest_${TARGET_ARCH}.zip" "${GOOGLE_DIR}/old/x265_${X265_VER}_${TARGET_ARCH}.zip"
+cp -f "$BUILD_DIR/temp/x265_latest_${TARGET_ARCH}.zip" "${ONEDRIVE_DIR}/old/x265_${X265_VER}_${TARGET_ARCH}.zip"
+# cp -f "$BUILD_DIR/temp/x265_latest_${TARGET_ARCH}.zip" "${DROPBOX_DIR}/old/x265_${X265_VER}_${TARGET_ARCH}.zip"
+
+echo ${X265_VER} > "${GOOGLE_DIR}/latest_build.txt"
+echo ${X265_VER} > "${ONEDRIVE_DIR}/latest_build.txt"
+echo ${X265_VER} > "${DROPBOX_DIR}/latest_build.txt"
+
+rm -rf $BUILD_DIR/temp

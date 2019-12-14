f880d90d0f4cd7d8aaf5d20e053907137c1662c4
rigaya <rigaya34589@live.jp>
2019-12-14 21:48:27 +0900

    ffmpeg_dll: launcherをVC2019向けに更新。

--- ffmpeg_dll/launcher/mingw32_vsvar.cmd
+++ ffmpeg_dll/launcher/mingw32_vsvar.cmd
@@ -1,6 +1,12 @@
 @echo off
 cd /d "%~dp0"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-call "%VS150COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvarsall.bat" x86
+
+rem VC2017
+rem call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+rem call "%VS150COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvarsall.bat" x86
+
+rem VC2019
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+call "%VS160COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvarsall.bat" x86
 
 "%~dp0msys2_shell.cmd" -mingw32 -use-full-path
--- ffmpeg_dll/launcher/mingw64_vsvar.cmd
+++ ffmpeg_dll/launcher/mingw64_vsvar.cmd
@@ -1,5 +1,12 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-call "%VS150COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvarsall.bat" x64
+cd /d "%~dp0"
+
+rem VC2017
+rem call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+rem call "%VS150COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+rem VC2019
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+call "%VS160COMNTOOLS%\..\..\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 "%~dp0msys2_shell.cmd" -mingw64 -use-full-path

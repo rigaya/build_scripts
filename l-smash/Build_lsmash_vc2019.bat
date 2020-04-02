a65bff1945788dfff61dface9584d288d7c0b1ea
rigaya <rigaya34589@live.jp>
2020-04-02 21:58:07 +0900

    lsmash: 64bit版のビルドを追加。

--- l-smash/Build_lsmash_vc2019.bat
+++ l-smash/Build_lsmash_vc2019.bat
@@ -3,17 +3,31 @@
 set TMP_PATH=F:\temp\lsmash_build
 set LSMASH_PATH=%TMP_PATH%\l-smash
 set BUILD_PROP_PATH=C:\ProgramEx\Build_lsmash.props
+set LSMASH_PATCH=C:\ProgramEx\lsmash.diff
 
 rd /s /q "%LSMASH_PATH%"
-git clone https://github.com/l-smash/l-smash.git "%LSMASH_PATH%"
+git clone -b fast https://github.com/nekopanda/l-smash.git "%LSMASH_PATH%"
 
 cd /d "%LSMASH_PATH%"
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat" x86
+set PLATFORM=Win32
+set ARCH=x86
+if "%1"=="x86" (
+    set PLATFORM=Win32
+    set ARCH=x86
+)
+if "%1"=="x64" (
+    set PLATFORM=x64
+    set ARCH=x64
+)
 
-MSBuild L-SMASH.sln /t:liblsmash /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=Win32;WholeProgramOptimization=true;ConfigurationType=StaticLibrary;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
-MSBuild L-SMASH.sln /t:cli /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=Win32;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
-MSBuild L-SMASH.sln /t:muxer /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=Win32;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
-MSBuild L-SMASH.sln /t:remuxer /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=Win32;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
-MSBuild L-SMASH.sln /t:boxdumper /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=Win32;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
-MSBuild L-SMASH.sln /t:timelineeditor /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=Win32;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
+git apply "%LSMASH_PATCH%"
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %ARCH%
+
+MSBuild L-SMASH.sln /t:liblsmash /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=%PLATFORM%;WholeProgramOptimization=true;ConfigurationType=StaticLibrary;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
+MSBuild L-SMASH.sln /t:cli /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=%PLATFORM%;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
+MSBuild L-SMASH.sln /t:muxer /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=%PLATFORM%;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
+MSBuild L-SMASH.sln /t:remuxer /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=%PLATFORM%;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
+MSBuild L-SMASH.sln /t:boxdumper /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=%PLATFORM%;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
+MSBuild L-SMASH.sln /t:timelineeditor /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=%PLATFORM%;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
--- l-smash/Build_lsmash_vc2019_auto_x64.bat
+++ l-smash/Build_lsmash_vc2019_auto_x64.bat
@@ -6,22 +6,23 @@ set OUTPUT_DIR_DROPBOX=C:\Users\rigaya\DropBox\L-SMASH
 set OUTPUT_DIR_ZOHO=C:\Users\rigaya\Zoho Docs\L-SMASH
 set ONDRIVEVIEW_PATH=C:\ProgramEx\OneDriveView\bin\OneDriveView.exe
 set TMP_PATH=F:\temp\build_lsmash
+set LSMASH_PATCH=C:\ProgramEx\lsmash.diff
 if exist "%TMP_PATH%" rmdir "%TMP_PATH%"
 md "%TMP_PATH%"
 cd /d "%TMP_PATH%"
 
-call %~dp0Build_lsmash_vc2019.bat x86
+call %~dp0Build_lsmash_vc2019.bat x64
 
 for /f usebackq %%a in (`git rev-list HEAD ^| find /c /v ""`) do set REV=%%a
 
-"%SEVENZIP_PATH%" a -xr!.git\* -xr!CLIRelease\* -t7z -mx=9 -mmt=off ..\L-SMASH-src.7z .\
+"%SEVENZIP_PATH%" a -xr!.git\* -xr!x64\CLIRelease\* -t7z -mx=9 -mmt=off ..\L-SMASH-src.7z .\
 
-set ARCHIEVE_NAME=L-SMASH rev%REV%.zip
-"%SEVENZIP_PATH%" a -tzip -mx=9 -mfb=256 -mpass=15 -mmt=off "..\%ARCHIEVE_NAME%" .\CLIRelease\*.exe LICENSE ..\L-SMASH-src.7z
-copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_ONEDRIVE%\L-SMASH latest.zip"
-copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_GDRIVE%\L-SMASH latest.zip"
-copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_DROPBOX%\L-SMASH latest.zip"
-copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_ZOHO%\L-SMASH latest.zip"
+set ARCHIEVE_NAME=L-SMASH rev%REV% x64.zip
+"%SEVENZIP_PATH%" a -tzip -mx=9 -mfb=256 -mpass=15 -mmt=off "..\%ARCHIEVE_NAME%" .\x64\CLIRelease\*.exe LICENSE ..\L-SMASH-src.7z
+copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_ONEDRIVE%\L-SMASH latest x64.zip"
+copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_GDRIVE%\L-SMASH latest x64.zip"
+copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_DROPBOX%\L-SMASH latest x64.zip"
+copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_ZOHO%\L-SMASH latest x64.zip"
 
 
 copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_ONEDRIVE%\L-SMASH_old"

174150a77d73fb99b95eafbcfa3562c017d23ea8
rigaya <rigaya34589@live.jp>
2020-04-04 11:48:28 +0900

    lsmash: ビルドスクリプトを見直し、x86/x64を常に両方ビルドするように。

--- l-smash/Build_lsmash_vc2019.bat
+++ l-smash/Build_lsmash_vc2019.bat
@@ -1,33 +1,18 @@
 @echo off
 
-set TMP_PATH=F:\temp\lsmash_build
+set TMP_PATH=F:\temp\build_lsmash
 set LSMASH_PATH=%TMP_PATH%\l-smash
 set BUILD_PROP_PATH=C:\ProgramEx\Build_lsmash.props
-set LSMASH_PATCH=C:\ProgramEx\lsmash.diff
 
-rd /s /q "%LSMASH_PATH%"
-git clone -b fast https://github.com/nekopanda/l-smash.git "%LSMASH_PATH%"
-
+if not exist "%LSMASH_PATH%" (
+    git clone -b add_ver_info https://github.com/rigaya/l-smash.git "%LSMASH_PATH%"
+)
 cd /d "%LSMASH_PATH%"
 
-set PLATFORM=Win32
-set ARCH=x86
-if "%1"=="x86" (
-    set PLATFORM=Win32
-    set ARCH=x86
+if "%~1" == "x86" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat" 
+    MSBuild -m L-SMASH.sln /property:Platform=Win32;Configuration="CLIRelease"
+) else (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 
+    MSBuild -m L-SMASH.sln /property:Platform=x64;Configuration="CLIRelease"
 )
-if "%1"=="x64" (
-    set PLATFORM=x64
-    set ARCH=x64
-)
-
-git apply "%LSMASH_PATCH%"
-
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %ARCH%
-
-MSBuild L-SMASH.sln /t:liblsmash /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=%PLATFORM%;WholeProgramOptimization=true;ConfigurationType=StaticLibrary;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
-MSBuild L-SMASH.sln /t:cli /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=%PLATFORM%;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
-MSBuild L-SMASH.sln /t:muxer /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=%PLATFORM%;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
-MSBuild L-SMASH.sln /t:remuxer /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=%PLATFORM%;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
-MSBuild L-SMASH.sln /t:boxdumper /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=%PLATFORM%;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
-MSBuild L-SMASH.sln /t:timelineeditor /property:WindowsTargetPlatformVersion=10.0;PlatformToolset=v142;Configuration="CLIRelease";Platform=%PLATFORM%;WholeProgramOptimization=true;ForceImportBeforeCppTargets="%BUILD_PROP_PATH%" /p:BuildProjectReferences=false
--- l-smash/Build_lsmash_vc2019_auto.bat
+++ l-smash/Build_lsmash_vc2019_auto.bat
@@ -3,50 +3,60 @@ set SEVENZIP_PATH=C:\Program Files\7-Zip\7z.exe
 set OUTPUT_DIR_ONEDRIVE=C:\Users\rigaya\OneDrive\L-SMASH
 set OUTPUT_DIR_GDRIVE=C:\Users\rigaya\GoogleDrive\L-SMASH
 set OUTPUT_DIR_DROPBOX=C:\Users\rigaya\DropBox\L-SMASH
-set OUTPUT_DIR_ZOHO=C:\Users\rigaya\Zoho Docs\L-SMASH
-set ONDRIVEVIEW_PATH=C:\ProgramEx\OneDriveView\bin\OneDriveView.exe
+set OUTPUT_DIR_WITH_PASS=Y:\temp
+set SEVEN_ZIP_PASS=rigaya
 set TMP_PATH=F:\temp\build_lsmash
-if exist "%TMP_PATH%" rmdir "%TMP_PATH%"
+set SUB_BAT=%~dp0Build_lsmash_vc2019.bat
+
+if exist "%TMP_PATH%" rmdir /s /q "%TMP_PATH%"
 md "%TMP_PATH%"
 cd /d "%TMP_PATH%"
 
-call %~dp0Build_lsmash_vc2019.bat x86
+call :BuildLSMASH x86
+call :BuildLSMASH x64
 
+cd "%TMP_PATH%\l-smash"
+set ARCHIEVE_SRC=L-SMASH-%REV%-src.7z
+del /s /q "%ARCHIEVE_SRC%"
+"%SEVENZIP_PATH%" a -xr!.git\* -xr!Win32\* -xr!x64\* -xr!CLIRelease\* -t7z -mx=9 -mmt=off "..\%ARCHIEVE_SRC%" .\
+copy /B /Y "..\%ARCHIEVE_SRC%" "%OUTPUT_DIR_ONEDRIVE%\src"
+copy /B /Y "..\%ARCHIEVE_SRC%" "%OUTPUT_DIR_GDRIVE%\src"
+copy /B /Y "..\%ARCHIEVE_SRC%" "%OUTPUT_DIR_DROPBOX%\src"
+exit /b
+
+:BuildLSMASH
+set ARCH=%1
+call "%SUB_BAT%" %ARCH%
+
+cd "%TMP_PATH%\l-smash"
 for /f usebackq %%a in (`git rev-list HEAD ^| find /c /v ""`) do set REV=%%a
 
-"%SEVENZIP_PATH%" a -xr!.git\* -xr!CLIRelease\* -t7z -mx=9 -mmt=off ..\L-SMASH-src.7z .\
+if "%ARCH%" == "x86" (
+    set PLATFORM=Win32
+) else (
+    set PLATFORM=x64
+)
+set ARCHIEVE_NAME=L-SMASH_rev%REV%_%ARCH%.zip
+del /s /q "..\%ARCHIEVE_NAME%"
+"%SEVENZIP_PATH%" a -tzip -mx=9 -mfb=256 -mpass=15 -mmt=off "..\%ARCHIEVE_NAME%" .\%PLATFORM%\CLIRelease\*.exe LICENSE
 
-set ARCHIEVE_NAME=L-SMASH rev%REV%.zip
-"%SEVENZIP_PATH%" a -tzip -mx=9 -mfb=256 -mpass=15 -mmt=off "..\%ARCHIEVE_NAME%" .\CLIRelease\*.exe LICENSE ..\L-SMASH-src.7z
-copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_ONEDRIVE%\L-SMASH latest.zip"
-copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_GDRIVE%\L-SMASH latest.zip"
-copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_DROPBOX%\L-SMASH latest.zip"
-copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_ZOHO%\L-SMASH latest.zip"
+del "%OUTPUT_DIR_ONEDRIVE%\L-SMASH_rev*_%ARCH%.zip"
+del "%OUTPUT_DIR_GDRIVE%\L-SMASH_rev*_%ARCH%.zip"
+del "%OUTPUT_DIR_DROPBOX%\L-SMASH_rev*_%ARCH%.zip"
 
+copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_ONEDRIVE%"
+copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_GDRIVE%"
+copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_DROPBOX%"
 
-copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_ONEDRIVE%\L-SMASH_old"
-copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_GDRIVE%\L-SMASH_old"
-copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_DROPBOX%\L-SMASH_old"
-copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_ZOHO%\L-SMASH_old"
+copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_ONEDRIVE%\old"
+copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_GDRIVE%\old"
+copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_DROPBOX%\old"
 
+set ARCHIEVE_NAME=L-SMASH_rev%REV%_%ARCH%_with_pass.7z
+del /s /q "..\%ARCHIEVE_NAME%"
+"%SEVENZIP_PATH%" a -t7z -p%SEVEN_ZIP_PASS% -mhe=on -mx=9 -mmt=off "..\%ARCHIEVE_NAME%" .\%PLATFORM%\CLIRelease\*.exe LICENSE
 
-echo %REV% > "%OUTPUT_DIR_ONEDRIVE%\latest_build.txt"
-echo %REV% > "%OUTPUT_DIR_GDRIVE%\latest_build.txt"
-echo %REV% > "%OUTPUT_DIR_DROPBOX%\latest_build.txt"
-echo %REV% > "%OUTPUT_DIR_ZOHO%\latest_build.txt"
+copy /B /Y "..\%ARCHIEVE_NAME%" "%OUTPUT_DIR_WITH_PASS%"
 
+exit /b
 
-REM "%ONDRIVEVIEW_PATH%" uploadraw "%OUTPUT_DIR_ONEDRIVE%\latest_build.txt" /L-SMASH/
-REM "%ONDRIVEVIEW_PATH%" uploadraw "%OUTPUT_DIR_ONEDRIVE%\L-SMASH latest.zip" /L-SMASH/
-REM "%ONDRIVEVIEW_PATH%" uploadraw "..\%ARCHIEVE_NAME%" /L-SMASH/
-REM "%ONDRIVEVIEW_PATH%" uploadraw "..\%ARCHIEVE_NAME%" /L-SMASH/L-SMASH_old/
-
-for /f "usebackq delims=" %%a in (`dir /b "%OUTPUT_DIR_ONEDRIVE%" ^| find "latest build"`) do set REMOVE_TXT=%%a
-del /q "%OUTPUT_DIR_ONEDRIVE%\%REMOVE_TXT%"
-
-type nul > "%OUTPUT_DIR_ONEDRIVE%\latest build is rev%REV%.txt"
-type nul > "%OUTPUT_DIR_GDRIVE%\latest build is rev%REV%.txt"
-type nul > "%OUTPUT_DIR_DROPBOX%\latest build is rev%REV%.txt"
-type nul > "%OUTPUT_DIR_ZOHO%\latest build is rev%REV%.txt"
-
-pause

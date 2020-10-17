7983f76f0a9d8254f157eb3cfeace4111746c09a
rigaya <rigaya34589@live.jp>
2020-10-17 18:27:44 +0900

    svtav1: gccビルドでもlto+pgoを有効に。

--- svtav1/build_svtav1.sh
+++ svtav1/build_svtav1.sh
@@ -1,13 +1,19 @@
 BUILD_DIR=`pwd`/build_svtav1
-BUILD_CCFLAGS="-Ofast -ffast-math -fomit-frame-pointer -fno-ident"
-BUILD_LDFLAGS="-static -static-libgcc -Wl,--gc-sections -Wl,--strip-all"
+BUILD_CCFLAGS="-Ofast -ffast-math -fomit-frame-pointer -flto"
+BUILD_LDFLAGS="-static -static-libgcc -flto -Wl,--gc-sections -Wl,--strip-all"
 MAKE_PROCESS=$NUMBER_OF_PROCESSORS
 #cmake.exeÇÃÇ†ÇÈèÍèä
 CMAKE_DIR="/C/Program Files/CMake/bin"
 PATCH_DIR=$HOME/patch
+PROFILE_GEN_CC="-fprofile-generate -fprofile-update=atomic"
+PROFILE_GEN_LD="-fprofile-generate -fprofile-update=atomic"
+PROFILE_USE_CC="-fprofile-use"
+PROFILE_USE_LD="-fprofile-use"
+YUV_PATH="/Y/QSVTest/sakura_op_8bit_10sec.yuv"
+YUV_10_PATH="/Y/QSVTest/sakura_op_10bit_10sec.yuv"
 
-export CC=clang
-export CXX=clang++
+export CC=gcc
+export CXX=g++
 export PATH="${CMAKE_DIR}:$PATH"
 
 #download
@@ -44,5 +50,39 @@ cp -r ../src/SVT-AV1 SVT-AV1
 cd $BUILD_DIR/$TARGET_ARCH/SVT-AV1
 mkdir build/msys2
 cd build/msys2
-cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DENABLE_NASM=ON -DCMAKE_ASM_NASM_COMPILER=yasm -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_C_FLAGS="${BUILD_CCFLAGS}" -DCMAKE_CXX_FLAGS="${BUILD_CCFLAGS}" -DCMAKE_EXE_LINKER_FLAGS="${BUILD_LDFLAGS}" ../..
-make SvtAv1EncApp -j8
+cmake -G "MSYS Makefiles" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DBUILD_TESTING=OFF \
+  -DENABLE_NASM=ON \
+  -DCMAKE_ASM_NASM_COMPILER=yasm \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+  -DCMAKE_C_FLAGS="${BUILD_CCFLAGS} ${PROFILE_GEN_CC}" \
+  -DCMAKE_CXX_FLAGS="${BUILD_CCFLAGS} ${PROFILE_GEN_CC}" \
+  -DCMAKE_EXE_LINKER_FLAGS="${BUILD_LDFLAGS} ${PROFILE_GEN_LD}" \
+  ../..
+
+make SvtAv1EncApp -j$MAKE_PROCESS
+
+../../Bin/Release/SvtAv1EncApp.exe -w 1280 -h 720 --qp 30 --fps-num 30 --fps-denom 1 -b /dev/nul -i "${YUV_PATH}"    --preset 5 -n 30
+../../Bin/Release/SvtAv1EncApp.exe -w 1280 -h 720 --qp 30 --fps-num 30 --fps-denom 1 -b /dev/nul -i "${YUV_PATH}"    --preset 7 -n 60
+../../Bin/Release/SvtAv1EncApp.exe -w 1280 -h 720 --qp 30 --fps-num 30 --fps-denom 1 -b /dev/nul -i "${YUV_PATH}"    --preset 5 -n 30 --pass 1 --stats pass.stats --aq-mode 2 --irefresh-type 2
+../../Bin/Release/SvtAv1EncApp.exe -w 1280 -h 720 --qp 30 --fps-num 30 --fps-denom 1 -b /dev/nul -i "${YUV_PATH}"    --preset 5 -n 30 --pass 2 --stats pass.stats --aq-mode 2 --irefresh-type 2
+../../Bin/Release/SvtAv1EncApp.exe -w 1280 -h 720 --qp 30 --fps-num 30 --fps-denom 1 -b /dev/nul -i "${YUV_10_PATH}" --preset 5 -n 30 --input-depth 10
+../../Bin/Release/SvtAv1EncApp.exe -w 1280 -h 720 --qp 30 --fps-num 30 --fps-denom 1 -b /dev/nul -i "${YUV_10_PATH}" --preset 7 -n 60 --input-depth 10
+../../Bin/Release/SvtAv1EncApp.exe -w 1280 -h 720 --qp 30 --fps-num 30 --fps-denom 1 -b /dev/nul -i "${YUV_10_PATH}" --preset 5 -n 30 --pass 1 --stats pass.stats --aq-mode 2 --irefresh-type 2 --input-depth 10 --hbd-md 2
+../../Bin/Release/SvtAv1EncApp.exe -w 1280 -h 720 --qp 30 --fps-num 30 --fps-denom 1 -b /dev/nul -i "${YUV_10_PATH}" --preset 5 -n 30 --pass 2 --stats pass.stats --aq-mode 2 --irefresh-type 2 --input-depth 10 --hbd-md 2
+
+cmake -G "MSYS Makefiles" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DBUILD_TESTING=OFF \
+  -DENABLE_NASM=ON \
+  -DCMAKE_ASM_NASM_COMPILER=yasm \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+  -DCMAKE_C_FLAGS="${BUILD_CCFLAGS} ${PROFILE_USE_CC}" \
+  -DCMAKE_CXX_FLAGS="${BUILD_CCFLAGS} ${PROFILE_USE_CC}" \
+  -DCMAKE_EXE_LINKER_FLAGS="${BUILD_LDFLAGS} ${PROFILE_USE_LD}" \
+  ../..
+
+make SvtAv1EncApp -j$MAKE_PROCESS

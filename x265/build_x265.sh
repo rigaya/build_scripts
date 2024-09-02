7b4389de0a2314824e8a3d68b4ab738c03edefe0
rigaya <rigaya34589@live.jp>
2024-09-02 18:57:01 +0900

    x265: a726d605 は不要になった。

--- x265/build_x265.sh
+++ x265/build_x265.sh
@@ -93,7 +93,6 @@ cp -r ../src/x265 .
 cd ${BUILD_DIR}/${TARGET_ARCH}/x265
 patch -p 1 < ~/patch/x265_version.diff
 patch -p 1 < ~/patch/x265_zone_param.diff
-patch -p 1 < ~/patch/x265_fix_unknown_frame.diff
 
 cd ${BUILD_DIR}/${TARGET_ARCH}/x265/build/msys
 
